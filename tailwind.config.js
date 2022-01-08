048ab5d9be91aa4fdc493e26aa2e1f7ae28eec1c
Feernandooff <ferobregon03@gmail.com>
2022-01-07 18:22:11 -0600

    Tailwid setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00bcd4',
+      },
+    },
+  },
+  plugins: [],
+};
